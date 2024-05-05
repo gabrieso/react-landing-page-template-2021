7388fa73af592d408f81d7569ab36f1707eb44ab
gabritosondi <135419990+gabritosondi@users.noreply.github.com>
2024-05-05 20:00:59 +0200

    Hotjar

--- src/components/Hotjar.tsx
+++ src/components/Hotjar.tsx
@@ -6,13 +6,13 @@ const HotjarTracking = ({ hotjarId, hotjarVersion }) => (
   <Helmet>
     <script>
       {`(function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:${hotjarId},hjsv:${hotjarVersion}};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
+                    h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                    h._hjSettings={hjid:4969754,hjsv:6};
+                    a=o.getElementsByTagName('head')[0];
+                    r=o.createElement('script');r.async=1;
+                    r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                    a.appendChild(r);
+                })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');`}
     </script>
   </Helmet>
 );
