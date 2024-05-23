072b44c83600efb48c5707eeaa867f1e83d141d8
gabritosondi <135419990+gabritosondi@users.noreply.github.com>
2024-05-23 09:31:34 +0200

    Join Now fixed

--- src/components/GoogleForm.tsx
+++ src/components/GoogleForm.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 
 const FeedbackForm = () => {
   return (
-    <div className="mt-10 p-8 bg-gray-100 shadow-lg rounded-lg border border-gray-300">
+    <div
+      className="mt-10 p-8 bg-gray-100 shadow-lg rounded-lg border border-gray-300"
+      id="form"
+    >
       <h2 className="text-3xl font-bold text-center text-primary my-4">
         Tell us your needs!!
       </h2>
--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -21,7 +21,7 @@ const MainHero = () => {
         <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
           <div className="rounded-md shadow">
             <Link
-              to="Pricing" // Set the ID of the target element
+              to="form" // Set the ID of the target element
               smooth={true}
               duration={500}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-background bg-primary hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
@@ -31,7 +31,7 @@ const MainHero = () => {
           </div>
           <div className="mt-3 sm:mt-0 sm:ml-3">
             <Link
-              to="Pricing" // Set another target element ID
+              to="form" // Set another target element ID
               smooth={true}
               duration={500}
               className={`w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md border-primary text-secondary bg-background hover:bg-border hover:text-primary md:py-4 md:text-lg md:px-10`}
